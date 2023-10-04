981f5b9cfc15f66ce0a1628c881869c15d154f39
Getange <emmanuelgetange48@gmail.com>
2023-10-04 21:00:08 +0300

    ADD: code refuctering

--- React-Challanges/pract-app/src/index.js
+++ React-Challanges/pract-app/src/index.js
@@ -17,33 +17,37 @@ function Card(){
         image="picture/image.jpg"
         name ="Emmanuel Getange"
         description="I'm a Full-stack Web developer. Master modern React from beginner to advanced! Context API, React Query, Redux Toolkit, Tailwind, advanced patterns"
+        html="HTML+CSS"
+        design="Web Design"
+        javascript="Javascript"
+        git="Git and Github"
+        react="React Js"
         />
     )
 }
 // component  to create a profile card
 function Profile(props){
-        return (
-            <main>
-                <h2>Profile Card</h2>
-                <div className='main-container'>
-                    <div className='main-siezing'>
-                        <img src={props.image} alt={props.name} className='main-image'></img>
-                    </div>
-
-                    <div className='main-description'>
-                        <h2>{props.name}</h2>
-                        <p className='main-siezing'>{props.description}</p>
-                        <div className='inline'>
-                            <p className='html'>HTML+CSS</p>
-                            <p className='javascript'>Javascript</p>
-                            <p className='design'>Web Design</p>
-                            <p className='git'>Git and Github</p>
-                            <p className='react'>React Js</p>
-                        </div>
+    return (
+        <main>
+            <h2>Profile Card</h2>
+            <div className='main-container'>
+                <div className='main-siezing'>
+                    <img src={props.image} alt={props.name} className='main-image'></img>
+                </div>
+                <div className='main-description'>
+                    <h2>{props.name}</h2>
+                    <p className='main-siezing'>{props.description}</p>
+                    <div className='inline'>
+                        <p className='html'>{props.html}</p>
+                        <p className='javascript'>{props.javascript}</p>
+                        <p className='design'>{props.design}</p>
+                        <p className='git'>{props.git}</p>
+                        <p className='react'>{props.react}</p>
                     </div>
                 </div>
-            </main>
-        )
+            </div>
+        </main>
+    )
 }
 const root = ReactDom.createRoot(document.getElementById('root'));
 root.render(
