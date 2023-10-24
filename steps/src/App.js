1312c62dadff6ad7a5267cd4a9252b405daf0941
Getange <emmanuelgetange48@gmail.com>
2023-10-25 02:39:11 +0300

    ADD: use of USESTATE to handle event listeners-interactivity

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages=[
     "Learn React ",
     "Apply Jobs",
@@ -5,21 +7,29 @@ const messages=[
 ];
 
 export default function App(){
-    const step=1;
+  // using states in components
+  const [step, setStep]=useState(1) ;
+
+//   console.log(arr) ;
+//  const  step=1
 
     // adding event handlers in react component
     function  handlePrevious(){
-        alert("Previous step ");
+      if(step>1){
+        setStep(step-1)
+      }
     }
     function  handleNext(){
-      alert("Next step ");
+      if(step<3){
+        setStep(step+1)
+      }
   }
     return (
       <div className="steps">
         <div className="numbers">
-          <div className={`${step>=1 ? 'active': ""}`}>1</div>
-          <div className={`${step>=2 ? 'active': ""}`}>2</div>
-          <div className={`${step>=3 ? 'active': ""}`}>3</div>
+          <div className={step>=1 ? 'active': ""}>1</div>
+          <div className={step>=2 ? 'active': ""}>2</div>
+          <div className={step>=3 ? 'active': ""}>3</div>
         </div>
 
         <p className="message">Step {step}:{messages[step-1]}</p>
