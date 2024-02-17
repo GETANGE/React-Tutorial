98064944825ad51471b72b73516f07a9879ba1eb
GETANGE <emmanuelgetange48@gmail.com>
2024-02-18 00:05:09 +0300

    Add: javascript in JSX with curly Braces

--- React-Challanges/pract-app/src/profile.js
+++ React-Challanges/pract-app/src/profile.js
@@ -1,3 +1,5 @@
+import { Fragment } from "react";
+
 export default function Profile(){
     return(
         <div>
@@ -7,10 +9,37 @@ export default function Profile(){
 }
 
 export function Profile1(){
+    // 1. USES OF CURLY BRACES
+    // Passing strings with quotes.
+    const avatar = "https://i.imgur.com/QIrZWGIs.jpg";
+    const description = "Alan L. Hart";
+
+    // a window into the javascript world
+    const name = "Emmanuel";
+
     return (
-        <img
-            src="https://i.imgur.com/QIrZWGIs.jpg"
-            alt="Alan L. Hart"
-        />
+        <Fragment>
+            <img
+                src={avatar}
+                alt= {description}
+            />
+            <p>{name} is an upcoming software developer</p>
+
+            {/* Using "double curlies" in css and other objects in JSX */}
+            <ul style={{
+                backgroundColor:'goldenrod',
+                color:'white'
+            }}>
+                <li>
+                    <a href="https://www.linkedin.com/in/emmanuel-hart/">LinkedIn</a>
+                </li>
+                <li>
+                    <a href="https://github.com/emmanuel-hart">GitHub</a>
+                </li>
+                <li>
+                    <a href="https://www.udemy.com/emmanuel-hart/">Udemy</a>
+                </li>
+            </ul>
+        </Fragment>
     );
 }
