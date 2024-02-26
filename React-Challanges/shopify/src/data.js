53c4d868e47dc99e80eb5ac2184f3835e6a28770
GETANGE <emmanuelgetange48@gmail.com>
2024-02-27 00:47:54 +0300

    ADD: Describing User Interface

--- React-Challanges/shopify/src/data.js
+++ React-Challanges/shopify/src/data.js
@@ -1,18 +1,56 @@
 export function Menu(){
     return(
-        <fragment>
-            <Pizza />
+        <fragment className="flex-container">
+            <Pizza 
+                name="Pizza Spinaci"
+                ingredients= "Tomato, mozarella, spinach, and ricotta cheese"
+                photoName="pizzas/spinaci.jpg"
+                price={300}
+            />
+            <Pizza 
+                name= "Focaccia"
+                photoName= "pizzas/focaccia.jpg"
+                ingredients= "Bread with italian olive oil and rosemary"
+                price ={200}
+            />
+            <Pizza 
+                name= "Pizza Margherita"
+                photoName= "pizzas/margherita.jpg"
+                ingredients= "Tomato and mozarella"
+                price ={150}
+            />
+            <Pizza 
+                name= "Pizza Funghi"
+                photoName= "pizzas/funghi.jpg"
+                ingredients= "Tomato, mozarella, mushrooms, and onion"
+                price ={150}
+            />
+            <Pizza 
+                name= "Pizza Salamino"
+                photoName= "pizzas/salamino.jpg"
+                ingredients= "Tomato, mozarella, and pepperoni"
+                price ={250}
+            />
+            <Pizza 
+                name= "Pizza Prosciutto"
+                photoName= "pizzas/prosciutto.jpg"
+                ingredients= "Tomato, mozarella, ham, aragula, and burrata cheese"
+                price ={80}
+            />
         </fragment>
     )
 }
 
-function Pizza(){
+function Pizza(props) {
     return(
         <div className="first-div">
-            <img src="pizzas/salamino.jpg" alt="Pizza Salamino" className="image"/>
-            <h3>Pizza Salamino</h3>
-            <p>Tomato and mozarella</p>
-            <p>10</p>
+            <div className="second-div">
+                <img src={props.photoName} alt="Pizza Salamino" className="image"/>
+                <h3>{props.name}</h3>
+                <p>{props.ingredients}</p>
+                <p style={{color:"green"}}>Ksh.{props.price} /=</p>
+            </div>
+            <button className="button2">Add to Cart</button>
         </div>
     )
 }
