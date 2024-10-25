62040cd1d6a710d58f58ab9bdc0b13ecf348f8c2
Getange <emmanuelgetange48@gmail.com>
2024-10-25 19:19:49 +0300

    ADD:initial commit

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+      <>
+      <div>
+        <img src="src/assets/shoe_display-removebg-preview.png" alt="" />
+      </div>
+      </>
+  )
+}
+
+export default App
